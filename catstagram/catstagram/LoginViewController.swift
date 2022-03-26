34b2152da0d376eab17b14c5e60c3d1f6005f1c2
YounKyum Jin <jinyounkyum@hufs.ac.kr>
2022-03-26 22:06:23 +0900

    로그인 화면 - 화면전환

--- catstagram/catstagram/LoginViewController.swift
+++ catstagram/catstagram/LoginViewController.swift
@@ -27,8 +27,18 @@ class LoginViewController: UIViewController {
         self.password = text
     }
     @IBAction func loginButtonDidTap(_ sender: UIButton) {
+        
     }
+    
     @IBAction func registerButtonDidTap(_ sender: UIButton) {
+        // 1. 스토리보드를 생성
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        // 2. ViewController 생성
+        let registerViewController = storyboard.instantiateViewController(withIdentifier: "RegisterVC") as! RegisterViewController
+        // 3. 화면전환 메소드 (present)를 사용하여 화면을 전환
+        //self.present(registerViewController, animated: true, completion: nil)
+        // 3-1. navigationController 이용하여 pushViewController로 화면 전환 가능
+        self.navigationController?.pushViewController(registerViewController, animated: true)
     }
     
 }
