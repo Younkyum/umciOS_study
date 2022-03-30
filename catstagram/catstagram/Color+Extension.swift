7b21c526d47bea69d7fa7ae1f41b5bafa990428a
YounKyum Jin <jinyounkyum@hufs.ac.kr>
2022-03-30 19:49:24 +0900

    유효성 검사 로직

---
+++ catstagram/catstagram/Color+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extension.swift
+//  catstagram
+//
+//  Created by Jin younkyum on 2022/03/30.
+//
+
+import UIKit
+
+extension UIColor {
+    static let facebookColor = UIColor(red: 0.255, green: 0.576, blue: 0.947, alpha: 1.0)
+    
+    static let disabledButtonColor = UIColor(red: 0.776, green: 0.872, blue: 0.980, alpha: 1.0)
+}
--- catstagram/catstagram/RegisterViewController.swift
+++ catstagram/catstagram/RegisterViewController.swift
@@ -10,86 +10,106 @@ import UIKit
 class RegisterViewController: UIViewController {
     // MARK: - Properties
     // 유효성 검사를 위한 프로퍼티
-    var isValidEmail = false
+    var isValidEmail = false {
+        didSet {
+            self.validateUserInfo()
+        }
+    }
     
-    var isValidName = false
+    var isValidName = false {
+        didSet {
+            self.validateUserInfo()
+        }
+    }
     
-    var isValidNickname = false
+    var isValidNickname = false {
+        didSet {
+            self.validateUserInfo()
+        }
+    }
     
-    var isValidPassword = false
-    
-    @IBOutlet weak var signupButton: UIButton!
+    var isValidPassword = false {
+        didSet {
+            self.validateUserInfo()
+        }
+    }
+   
     
     // TextField
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBOutlet weak var nameTextField: UITextField!
     
-    @IBOutlet weak var nicknameTextField: UITextField!
+    @IBOutlet weak var nickNameTextField: UITextField!
     
-    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordTextFiled: UITextField!
     
-    var textField: [UITextField] {
-        [emailTextField, nameTextField, nicknameTextField, passwordTextField]
-    }
+    @IBOutlet weak var signupButton: UIButton!
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUpTextField()
     }
     
     //MARK: - Action
-    @objc
-    func textFieldEditingChanged(_ sender : UITextField) {
+    @objc func textFieldEditingChanged(_ sender: UITextField) {
         let text = sender.text ?? ""
         
         switch sender {
         case emailTextField:
-            print("email")
+            self.isValidEmail = text.isValidEmail()
         case nameTextField:
-            print("name")
-        case nicknameTextField:
-            print("nickname")
-        case passwordTextField:
-            print("password")
+            self.isValidName = text.count > 2
+        case nickNameTextField:
+            self.isValidNickname = text.count > 2
+        case passwordTextFiled:
+            self.isValidPassword = text.isValidPassWord()
         default:
-            fatalError("Missing TextField...")
+            print("help")
         }
     }
     
     
     //MARK: - Helpers
-    private func setupTextField() {
-        textField.forEach() { tf in
-            tf.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
-        }
-    
+    private func setUpTextField() {
+        emailTextField.addTarget(self,
+                                 action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
+        nameTextField.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
+        nickNameTextField.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
+        passwordTextFiled.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
+        
     }
+
     // 사용자가 입력한 회원정보 확인하고 -> UI 업데이트
     private func validateUserInfo() {
-        if isValidEmail
-            && isValidName
-            && isValidNickname
-            && isValidPassword {
-            self.signupButton.backgroundColor = UIColor.fa
+        if isValidEmail && isValidName && isValidNickname && isValidPassword {
+            
+            UIView.animate(withDuration: 0.33, animations: {
+                self.signupButton.backgroundColor = UIColor.facebookColor
+            })
+            
         } else {
-            self.signupButton.backgroundColor = disabledColor
+            UIView.animate(withDuration: 0.33, animations: {
+                self.signupButton.backgroundColor = UIColor.disabledButtonColor
+            })
         }
     }
+    
     
 }
 
 extension String {
     func isValidPassWord() -> Bool {
-        let regularExpression = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}"
-        let passwordValidation = NSPredicate.init(format: "SELF MATCHES %@", regularExpression)
+        let regex = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,50}" // 8자리 ~ 50자리 영어+숫자+특수문자
+        let passwordValidation = NSPredicate.init(format: "SELF MATCHES %@", regex)
         return passwordValidation.evaluate(with: self)
     }
     
     func isValidEmail() -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]\\.[A-Za-z]{2,64}"
-        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", regex)
         return emailTest.evaluate(with: self)
     }
 }
